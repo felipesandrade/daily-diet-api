c186290194cfbc33419dcc150772ea5daf93b72e
Felipe Andrade <felipes.andrade@gmail.com>
2025-04-16 06:24:01 -0300

    Updating model name for meal.py

--- models/meal.py
+++ models/meal.py
@@ -1,8 +1,8 @@
 from database import db
 
-class Diet(db.Model):
+class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     date_time = db.Column(db.DateTime, nullable=False)
-    is_diet = db.Column(db.Boolean, nullable=False, default=False)
+    on_diet = db.Column(db.Boolean, nullable=False, default=False)
