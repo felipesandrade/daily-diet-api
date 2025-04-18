8febbea051f6f1925cc70de2f23b66b0ee6d41d7
Felipe Andrade <felipes.andrade@gmail.com>
2025-04-18 20:59:02 -0300

    Updating column names in meal table

--- app.py
+++ app.py
@@ -25,33 +25,67 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    user_name = data.get("user_name")
+    password = data.get("password")
+
+    if user_name and password:
+        user = User.query.filter_by(user_name=user_name).first()
+
+        if not user:
+            if 'user_name' in data:
+                if not isinstance(user_name, str) and not user_name.strip():
+                    return jsonify({"erro": "Usuário inválido."}), 400
+                user_name = user_name.strip()
+            if 'password' in data:
+                if not isinstance(password, str) and not password.strip():
+                    return jsonify({"erro": "Password inválida."}), 400
+                password = password.strip()
+
+            user = User(user_name=user_name, password=password, role="user")
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"message": "Usuário criado com sucesso."}), 201
+        
+        return jsonify({"message": "Usuário já cadastrado."}), 404
+    
+    return jsonify({"message": "Dados inválidos."}), 400
+
 @app.route('/meal', methods=['POST'])
 def create_meal():
     data = request.json
+    meal_name = data.get("meal_name")
+    meal_description = data.get("meal_description")
+    meal_date_time = data.get("meal_date_time")
+    meal_on_diet = data.get("meal_on_diet")
 
-    if 'name'in data:
-        if not isinstance(data.get("name"), str) or not data.get("name").strip():
-            return jsonify({"erro": "Nome inválido."}), 400
-        name = data.get("name").strip()
-    if 'description' in data:
-        if not isinstance(data.get("description"), str) or not data.get("description").strip():
-            return jsonify({"erro": "Descrição inválida."}), 400
-        description = data.get("description").strip()
-    if 'date_time' in data:
-        date_time = data.get("date_time")
-        try:
-            formatted_datetime = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
-        except ValueError: 
-            return jsonify({"erro": "Data e hora inválidos."}), 400
-    if 'on_diet' in data:
-        on_diet = data.get("on_diet") if data.get("on_diet") else False
-        if not isinstance(on_diet, bool):
-            return jsonify({"erro": "Na dieta inválido."}), 400
+    if meal_name and meal_description and meal_date_time and meal_on_diet:
 
-    meal = Meal(name=name, description=description, date_time=formatted_datetime, on_diet=on_diet)
-    db.session.add(meal)
-    db.session.commit()
-    return jsonify({"message": "Refeição criada com sucesso."}), 201
+        if 'meal_name'in data:
+            if not isinstance(meal_name, str) or not meal_name.strip():
+                return jsonify({"erro": "Nome inválido."}), 400
+            meal_name = meal_name.strip()
+        if 'meal_description' in data:
+            if not isinstance(meal_description, str) or not meal_description.strip():
+                return jsonify({"erro": "Descrição inválida."}), 400
+            meal_description = meal_description.strip()
+        if 'meal_date_time' in data:
+            try:
+                formatted_meal_date_time = datetime.strptime(meal_date_time, "%Y-%m-%d %H:%M:%S")
+            except ValueError: 
+                return jsonify({"erro": "Data e hora inválidos."}), 400
+        if 'meal_on_diet' in data:
+            if not isinstance(meal_on_diet, bool):
+                return jsonify({"erro": "Refeição na dieta inválido."}), 400
+
+        meal = Meal(meal_name=meal_name, meal_description=meal_description, meal_date_time=formatted_meal_date_time, meal_on_diet=meal_on_diet)
+        db.session.add(meal)
+        db.session.commit()
+        return jsonify({"message": "Refeição criada com sucesso."}), 201
+
+    return jsonify({"message": "Dados inválidos."}), 400
 
 @app.route('/meal', methods=['GET'])
 def get_meals():
@@ -60,13 +94,13 @@ def get_meals():
     if meals:
         meals_list = []
         for meal in meals:
-            formatted_datetime = meal.date_time.strftime("%Y-%m-%d %H:%M:%S")
+            formatted_meal_date_time = meal.meal_date_time.strftime("%Y-%m-%d %H:%M:%S")
             meals_list.append({
                 "id": meal.id,
-                "name": meal.name,
-                "description": meal.description,
-                "date_time": formatted_datetime,
-                "on_diet": meal.on_diet
+                "meal_name": meal.meal_name,
+                "meal_description": meal.meal_description,
+                "meal_date_time": formatted_meal_date_time,
+                "meal_on_diet": meal.meal_on_diet
             })
         return jsonify(meals_list), 200
     return jsonify({"message": "Nenhuma refeição encontrada."}), 400
@@ -76,13 +110,13 @@ def get_meal(meal_id):
     meal = Meal.query.get(meal_id)
 
     if meal:
-        formatted_datetime = meal.date_time.strftime("%Y-%m-%d %H:%M:%S")
+        formatted_meal_date_time = meal.meal_date_time.strftime("%Y-%m-%d %H:%M:%S")
         meal_data = {
             "id": meal.id,
-            "name": meal.name,
-            "description": meal.description,
-            "date_time": formatted_datetime,
-            "on_diet": meal.on_diet
+            "meal_name": meal.meal_name,
+            "meal_description": meal.meal_description,
+            "meal_date_time": formatted_meal_date_time,
+            "meal_on_diet": meal.meal_on_diet
         }
         return jsonify(meal_data), 200
     return jsonify({"message": "Refeição não encontrada."}), 404
@@ -95,28 +129,31 @@ def update_meal(meal_id):
         return jsonify({"message": "Refeição não econtrada."}), 404
         
     data = request.json
+    meal_name = data.get("meal_name")
+    meal_description = data.get("meal_description")
+    meal_date_time = data.get("meal_date_time")
+    meal_on_diet = data.get("meal_on_diet")
 
-    if 'name' in data:
-        if not isinstance(data.get("name"), str) or not data.get("name").strip():
+
+    if 'meal_name' in data:
+        if not isinstance(meal_name, str) or not meal_name.strip():
             return jsonify({"erro": "Nome inválido."}), 400
-        meal.name = data.get("name").strip()
+        meal.meal_name = meal_name.strip()
 
-    if 'description' in data:
-        if not isinstance(data.get("description"), str) or not data.get("description").strip():
+    if 'meal_description' in data:
+        if not isinstance(meal_description, str) or not meal_description.strip():
             return jsonify({"erro": "Descrição inválida."}), 400
-        meal.description = data.get("description").strip()
-    if 'date_time' in data:
-        date_time = data.get("date_time")
+        meal.meal_description = meal_description.strip()
+    if 'meal_date_time' in data:
         try:
-            formatted_datetime = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+            formatted_meal_date_time = datetime.strptime(meal_date_time, "%Y-%m-%d %H:%M:%S")
         except ValueError:
             return jsonify({"erro": "Data e hora inválidos."}), 400
-        meal.date_time = formatted_datetime
-    if 'on_diet' in data:
-        on_diet = data.get("on_diet")
-        if not isinstance(on_diet, bool):
+        meal.meal_date_time = formatted_meal_date_time
+    if 'meal_on_diet' in data:
+        if not isinstance(meal_on_diet, bool):
             return jsonify({"erro": "Na diera inválida."}), 400
-        meal.on_diet = on_diet
+        meal.meal_on_diet = meal_on_diet
 
     db.session.commit()
     return jsonify({"message": "Refeição alterada com sucesso."}), 200
--- models/meal.py
+++ models/meal.py
@@ -2,7 +2,7 @@ from database import db
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
-    date_time = db.Column(db.DateTime, nullable=False)
-    on_diet = db.Column(db.Boolean, nullable=False, default=False)
+    meal_name = db.Column(db.String(80), nullable=False)
+    meal_description = db.Column(db.String(255), nullable=False)
+    meal_date_time = db.Column(db.DateTime, nullable=False)
+    meal_on_diet = db.Column(db.Boolean, nullable=False, default=False)
